d03c4fa6911afb8dbbc84f4e99db33355a1d241a
Hamed Hafezi <h.hafezi93@gmail.com>
2020-02-27 00:00:47 +0330

    Profile feed

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -3,9 +3,19 @@ from rest_framework import permissions
 class UpdateOwnProfile(permissions.BasePermission):
     '''Allow user to edit their own profile'''
 
-    def has_object_permission(self, requset, view, obj):
+    def has_object_permission(self, request, view, obj):
         print("=============>", obj)
         '''Check user is trying to edit their own profile'''
-        if requset.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.id == requset.user.id
+        return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    '''Allow users to update their own status'''
+
+    def has_object_permission(self, request, view, obj):
+        '''Check the user is trying update their own status'''
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile-viewset', views.UserProdileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('login', views.UserLoginApiView.as_view()),
